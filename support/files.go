39fe2ef1805418839997d72bb9f8cfdc8cc433be
Matt Townsend <mt76@gmx.com>
2024-03-20 23:01:24 +0000

    Add file handling functions

--- support/files.go
+++ support/files.go
@@ -9,20 +9,30 @@ import (
 	lang "github.com/mt1976/mockterm/language"
 )
 
+// GetFilesList returns a list of files in the specified base folder.
+// If an error occurs, an error message is displayed using the provided terminal.
 func GetFilesList(crt term.Crt, baseFolder string) []os.DirEntry {
 	files, err := os.ReadDir(baseFolder)
 	if err != nil {
-		//fmt.Printf("%s Error reading folder %s: %v\n", chNormal, baseFolder, err)
 		crt.Error(errs.ErrReadingFolder, baseFolder, err.Error())
 		return nil
 	}
 	return files
 }
 
+// GetTimeStamp returns the current date in the format "20060102".
 func GetTimeStamp() string {
 	return time.Now().Format("20060102")
 }
 
+// OpenFile opens a file for writing.
+//
+// Parameters:
+// t - the terminal to use for output.
+// filename - the name of the file to open.
+//
+// Returns:
+// a file pointer, or an error if any occurred while opening the file.
 func OpenFile(t term.Crt, filename string) (*os.File, error) {
 	file, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
@@ -32,6 +42,15 @@ func OpenFile(t term.Crt, filename string) (*os.File, error) {
 	return file, nil
 }
 
+// WriteStringSliceToFile writes a slice of strings to a file.
+//
+// Parameters:
+// t - the terminal to use for output.
+// file - the file to write to.
+// content - the slice of strings to write to the file.
+//
+// Returns:
+// an error if any occurred while writing to the file.
 func WriteStringSliceToFile(t term.Crt, file *os.File, content []string) error {
 	for _, line := range content {
 		_, err := file.WriteString(line + lang.SymNewline)
