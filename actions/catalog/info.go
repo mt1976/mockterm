42a98879f2ee67ae139b4433614132b81308e6be
Matt Townsend <mt76@gmx.com>
2024-04-01 08:30:39 +0100

    Refactor info struct methods to use term.Page instead of term.ViewPort

--- actions/catalog/info.go
+++ actions/catalog/info.go
@@ -16,25 +16,28 @@ func New() info {
 }
 
 // breakData adds a new section to the info struct with the given title and adds a line break
-func (i *info) breakData(t *term.ViewPort, title string) {
+func (i *info) breakData(p *term.Page, title string) {
+	t := p.ViewPort()
 	// add a new section to the info struct
 	i.data = append(i.data, t.Format("", ""))
 	i.data = append(i.data, t.Format(title, ""))
 	i.data = append(i.data, t.Format("", ""))
 	// print the given title in bold
-	t.Print(t.Formatters.Bold(title))
+	p.Add(t.Formatters.Bold(title), "", "")
 	// add a line break
-	t.Break()
+	p.AddBlankRow()
 	results = append(results, title)
 }
 
 // storeData adds a new key-value pair to the info struct as a new section with the given title and data
-func (i *info) storeData(t *term.ViewPort, title string, data string) {
+func (i *info) storeData(p *term.Page, title string, data string) {
+	t := p.ViewPort()
 	// Padd title to 15 characters
 	title = fmt.Sprintf("%-30s", title)
 	// add a new section to the info struct
 	i.data = append(i.data, t.Format(title+": "+data, ""))
 	//print the given title with data in bold
-	t.Print(title + ": " + t.Formatters.Bold(data))
+	//t.Print(title + ": " + t.Formatters.Bold(data))
+	p.AddFieldValuePair(title, data)
 	results = append(results, title+": "+data)
 }
